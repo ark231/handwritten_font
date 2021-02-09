d50d8512ef44754a4f56ae8a52eb7a8f6f33db79
ark314 <arklp20909@gmail.com>
2021-02-09 18:21:16 +0900

    add: 文字定義ファイルの文法の追加に対応

--- chardefs/prog_codes/parse_chardef.hpp
+++ chardefs/prog_codes/parse_chardef.hpp
@@ -1,33 +1,28 @@
 #ifndef HANDFONT_PARSE_CHARDEF
 #define HANDFONT_PARSE_CHARDEF
 #include<vector>
-#include<filesystem>
+#include"chardef_consts.hpp"
+#include"chardef_filemeta.hpp"
 namespace handfont{
-	enum class grid_size{
-		SMALL,
-		LARGE
+	struct other_char{
+		chardef_filemeta filemeta;
+		Unicode character;
+		void set_all(Unicode,filecode,std::string);
 	};
-	enum class char_width{
-		HALF,
-		FULL
-	};
-	enum class index_grid_id{
-		LEFT=0,
-		RIGHT,
-		NUM_ALL
-	};
-	using UInt=unsigned int;
-	using Unicode=uint32_t;
 	struct character_info{
 		Unicode character;
 		char_width width;
+		guide_type g_type;
 		UInt Internal_Grid_IDs[(int)index_grid_id::NUM_ALL];
+		bool has_alternative;
+		other_char alternative;
 	};
 	struct chardef_data{
 		grid_size size;
+		font_type f_type;
 		std::vector<std::string> char_typenames;
 		std::vector<character_info> char_infos;
-		void parse_chardef(std::filesystem::path);
+		void parse_chardef(chardef_filemeta);
 	};
 }
 #endif
