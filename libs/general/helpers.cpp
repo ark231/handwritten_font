6e0c82edcaeb405af46ca65c1cf1377f659778fd
ark314 <arklp20909@gmail.com>
2021-03-16 22:48:42 +0900

    add: よく使うenumとstringの変換関数を追加

--- libs/general/helpers.cpp
+++ libs/general/helpers.cpp
@@ -3,6 +3,7 @@
 #include<string>
 
 #include"helpers.hpp"
+#include"chardef/chardef_consts.hpp"
 namespace handfont{
 	std::string to_hex(int num){
 		std::stringstream result;
@@ -13,5 +14,47 @@ namespace handfont{
 		std::stringstream result;
 		result<<std::setw(width)<<std::setfill(fillchar)<<std::hex<<num;
 		return result.str();
+	}
+	std::string to_string(const grid_size size){
+		std::string result;
+		switch(size){
+			case grid_size::SMALL:
+				result = "small";
+				break;
+			case grid_size::LARGE:
+				result = "large";
+				break;
+		}
+		return result;
+	}
+	std::string to_string(const font_type type){
+		std::string result;
+		switch(type){
+			case font_type::MONO:
+				result = "mono";
+				break;
+			case font_type::PROPORTIONAL:
+				result = "proportional";
+				break;
+		}
+		return result;
+	}
+	grid_size to_grid_size(const std::string input){
+		grid_size result;
+		if(input == "small"){
+			result = grid_size::SMALL;
+		}else if(input == "large"){
+			result = grid_size::LARGE;
+		}
+		return result;
+	}
+	font_type to_font_type(const std::string input){
+		font_type result;
+		if(input == "mono"){
+			result = font_type::MONO;
+		}else if(input == "proportional"){
+			result = font_type::PROPORTIONAL;
+		}
+		return result;
 	}
 }
--- libs/general/helpers.hpp
+++ libs/general/helpers.hpp
@@ -1,8 +1,13 @@
 #ifndef HANDFONT_HELPERS
 #define HANDFONT_HELPERS
 #include<string>
+#include"chardef/chardef_consts.hpp"
 namespace handfont{
 	std::string to_hex(int);
 	std::string to_hex(int,int,const char);
+	std::string to_string(const grid_size);
+	std::string to_string(const font_type);
+	grid_size to_grid_size(const std::string);
+	font_type to_font_type(const std::string);
 }
 #endif
