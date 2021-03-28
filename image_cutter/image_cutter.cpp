0c1ecf2a2d1076634d31949df8a3278d3f7477dc
ark314 <arklp20909@gmail.com>
2021-03-28 19:27:59 +0900

    add: 切り分けられた候補から、QRコードを読み取り。

--- image_cutter/image_cutter.cpp
+++ image_cutter/image_cutter.cpp
@@ -15,6 +15,8 @@
 namespace bpo=boost::program_options;
 namespace stdfsys=std::filesystem;
 
+void writeto_file(stdfsys::path outfilepath,cv::Mat);
+
 int main(int argc,char *argv[]){
 	bpo::options_description opt("option");
 	opt.add_options()
@@ -85,6 +87,7 @@ int main(int argc,char *argv[]){
 		cv::Mat image_dialate(image.size(),CV_8UC1);
 		cv::Mat element = cv::getStructuringElement( cv::MORPH_RECT,cv::Size(block_size,block_size));
 		cv::erode( image_blurred, image_dialate, element );
+		/*
 		auto outfilepath_erode = (project_dir/"output"/(filepath.path().filename().native()+"_blue_erode.png"));
 		try{
 			cv::imwrite(outfilepath_erode.native(),image_dialate);
@@ -95,6 +98,7 @@ int main(int argc,char *argv[]){
 			std::exit(EXIT_FAILURE);
 		}
 		std::cout<<"successfully written into "<<outfilepath_erode.native()<<std::endl;
+		*/
 
 		cv::Mat image_edge(image.size(),CV_8UC1);
 
@@ -111,7 +115,8 @@ int main(int argc,char *argv[]){
 			//cv::approxPolyDP( contours[i], contours_poly[i], 3, true );
 			contours_poly[i] = contours[i];
 			auto bound_rect_tmp = cv::boundingRect(contours_poly[i]);
-			if(bound_rect_tmp.area() > 15*dpmm*15*dpmm){
+			/*ignore too small or too big rectangles*/
+			if(15*dpmm*15*dpmm < bound_rect_tmp.area() && bound_rect_tmp.area() < image_blue.rows*image_blue.cols*(90/100.0)){
 				boundRects.push_back(bound_rect_tmp);
 			}
 		}
@@ -123,14 +128,44 @@ int main(int argc,char *argv[]){
 			cv::drawContours(result, contours, (int)i, color);
 			//cv::rectangle(result,boundRects[i].tl(),boundRects[i].br(),color,3);
 		}
-		for(const auto& boundRect : boundRects){
-			cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256));
-			cv::rectangle(result,boundRect.tl(),boundRect.br(),color,3);
-		}
-		/*
 		cv::QRCodeDetector qr_detector;
 		std::vector<std::string> qr_infos;
-		//std::vector<cv::Point> qr_vertices;
+		std::vector<std::vector<cv::Point>> qr_vertices;// multi 'multi vertex'
+		//int counta_qr_area=0;
+		std::cout<<"start detecting qr codes"<<std::endl;
+		for(const auto& boundRect : boundRects){
+			/*
+			cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0,256), rng.uniform(0,256));
+			cv::rectangle(result,boundRect.tl(),boundRect.br(),color,3);
+			*/
+			//cv::Mat qr_area(image_blue,boundRect);
+			cv::Mat qr_area(image_bin,boundRect);
+			cv::Mat qr_area_border(qr_area.rows+2*(5.0*dpmm),qr_area.cols+2*(5.0*dpmm),CV_8UC1);
+			cv::copyMakeBorder(qr_area,qr_area_border,5.0*dpmm,5.0*dpmm,5.0*dpmm,5.0*dpmm,cv::BORDER_CONSTANT,0xff);
+			std::vector<cv::Point> qr_vertice;
+			qr_infos.push_back(qr_detector.detectAndDecode(qr_area_border,qr_vertice));
+			qr_vertices.push_back(qr_vertice);
+			/*
+			writeto_file((project_dir/"output"/(filepath.path().filename().native()+"_blue_qr"+std::to_string(counta_qr_area)+".png")),qr_area_border);
+			counta_qr_area++;
+			*/
+		}
+		int num_qr_vertice=0;
+		for(const auto& qr_vertice : qr_vertices){
+			num_qr_vertice+=qr_vertice.size();
+		}
+		std::cout<<std::to_string(num_qr_vertice)<<std::endl;
+		//if(qr_vertices.size() < 4*4){
+		if(num_qr_vertice < 4*4){
+			std::cerr<<"error: couldn't detect nor decode enough qr codes in the input image!"<<std::endl;
+			continue;
+		}
+		for(const auto& qr_info : qr_infos){
+			if(!qr_info.empty()){
+				std::cout<<qr_info<<std::endl;
+			}
+		}
+		/*
 		cv::Mat qr_vertices;
 		std::cout<<"start detecting and decoding qr codes"<<std::endl;
 		qr_detector.detectAndDecodeMulti(image_bin,qr_infos,qr_vertices);
@@ -138,17 +173,20 @@ int main(int argc,char *argv[]){
 			std::cerr<<"error: couldn't detect nor decode qr codes in the input image!"<<std::endl;
 			continue;
 		*/
-		auto outfilepath = (project_dir/"output"/(filepath.path().filename().native()+"_blue_edge.png"));
-		try{
-			cv::imwrite(outfilepath.native(),result);
-		}
-		catch(cv::Exception excep){
-			std::cerr<<"error: couldn't write image to "<<outfilepath.native()<<std::endl;
-			std::cerr<<excep.what()<<std::endl;
-			std::exit(EXIT_FAILURE);
-		}
-		std::cout<<"successfully written into "<<outfilepath.native()<<std::endl;
+		writeto_file((project_dir/"output"/(filepath.path().filename().native()+"_blue_edge.png")),result);
 
 	}
 	return 0;
+}
+
+void writeto_file(stdfsys::path outfilepath,cv::Mat image){
+	try{
+		cv::imwrite(outfilepath.native(),image);
+	}
+	catch(cv::Exception excep){
+		std::cerr<<"error: couldn't write image to "<<outfilepath.native()<<std::endl;
+		std::cerr<<excep.what()<<std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+	std::cout<<"successfully written into "<<outfilepath.native()<<std::endl;
 }
