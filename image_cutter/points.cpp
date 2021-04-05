078240d3a2a5e3c943497ad4ff8ecde0c13674d6
ark314 <arklp20909@gmail.com>
2021-04-05 15:11:19 +0900

    change: cv::Pointを継承

--- image_cutter/points.cpp
+++ image_cutter/points.cpp
@@ -1,23 +1,28 @@
 #include"points.hpp"
 namespace handfont{
 	localPoint::localPoint(cv::Point point,cv::Point origin){
-		this->point = point;
+		//this->point = point;
+		this->x = point.x;
+		this->y = point.y;
 		this->origin = origin;
 	}
+	/*
 	cv::Point localPoint::get_point() const noexcept{
 		return point;
 	}
+	*/
 	cv::Point localPoint::get_origin() const noexcept{
 		return origin;
 	}
-	bool operator ==(const localPoint& one,const localPoint& theother){
+	bool operator==(const localPoint& one,const localPoint& theother){
 		if(one.get_origin() != theother.get_origin()){
 			return false;// 例外のほうがわかりやすいか？
 		}else{
-			return one.get_point() == theother.get_point();
+			return (one.x == theother.x) && (one.y == theother.y);
 		}
 	}
 	globalPoint::globalPoint(cv::Point point){
-		this->point = point;
+		this->x = point.x;
+		this->y = point.y;
 	}
 }
--- image_cutter/points.hpp
+++ image_cutter/points.hpp
@@ -3,29 +3,44 @@
 #include<opencv2/core.hpp>
 namespace handfont{
 	class localPoint;
-	class globalPoint {
-		cv::Point point;
+	class globalPoint : public cv::Point{
+		//cv::Point point;
 		public:
 		globalPoint(cv::Point);
+		globalPoint(){}
 		operator localPoint() = delete;
+		/*
 		operator cv::Point() const noexcept{
 			return point;
 		};
+		*/
 	};
-	class localPoint{
-		cv::Point point;
+	template<class P>
+	bool Point_x_less(const P& one,const P& theother){
+		//return ((cv::Point)one).x < ((cv::Point)theother).x;
+		return one.x < theother.x;
+	}
+	template<class P>
+	bool Point_y_less(const P& one,const P& theother){
+		//return ((cv::Point)one).y < ((cv::Point)theother).y;
+		return one.y < theother.y;
+	}
+	class localPoint : public cv::Point{
+		//cv::Point point;
 		cv::Point origin;
 		public:
 		localPoint(cv::Point,cv::Point);
 		localPoint(){};
 		cv::Point get_origin() const noexcept;
-		cv::Point get_point() const noexcept;
+		//cv::Point get_point() const noexcept;
 		explicit operator globalPoint() const noexcept{
-			return globalPoint(origin+point);
+			return globalPoint(origin+cv::Point(x,y));
 		};
+		/*
 		explicit operator cv::Point() const noexcept{
 			return point;
 		}
+		*/
 	};
 	bool operator ==(const localPoint&,const localPoint&);
 }
