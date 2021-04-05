943b0daa99c415e8d2545bf043724c9d0cec6f54
ark314 <arklp20909@gmail.com>
2021-04-05 15:09:37 +0900

    change: テンプレートの実装をヘッダファイルに

--- image_cutter/sides.cpp
+++ image_cutter/sides.cpp
@@ -3,32 +3,4 @@
 #include<opencv2/core.hpp>
 #include"sides.hpp"
 namespace handfont{
-	template<class P>
-	side<P>::side(P start,P end){
-		this->start = start;
-		this->end = end;
-	}
-	template<class P>
-	double side<P>::length(){
-		return std::sqrt(std::pow(((cv::Point)end).x-((cv::Point)start).x,2)+std::pow(((cv::Point)end).y-((cv::Point)start).y,2));
-	}
-
-	template <class P>
-	P same_points(side<P> one,side<P> theother){
-		if(one.start == theother.start){
-			return one.start;
-		}else if(one.start == theother.end){
-			return one.start;
-		}else if(one.end == theother.start){
-			return one.end;
-		}else if(one.end == theother.end){
-			return one.end;
-		}else{
-			throw std::runtime_error("given two sides doesn't share any points");
-		}
-	}
-	template <class P>
-	bool operator <(side<P>& one,side<P>& theother){
-		return one.length() < theother.length();
-	}
 }
--- image_cutter/sides.hpp
+++ image_cutter/sides.hpp
@@ -5,13 +5,32 @@ namespace handfont{
 	struct side{
 		P start;
 		P end;
-		side(P,P);
-		double length();
+		side(P start,P end){
+			this->start = start;
+			this->end = end;
+		}
+		double length(){
+			//return std::sqrt(std::pow(((cv::Point)end).x-((cv::Point)start).x,2)+std::pow(((cv::Point)end).y-((cv::Point)start).y,2));
+			return std::sqrt(std::pow(end.x-start.x,2)+std::pow(end.y-start.y,2));
+		}
 	};
 	template <class P>
-	P same_points(side<P>,side<P>);
+	P same_points(side<P> one,side<P> theother){
+		if(one.start == theother.start){
+			return one.start;
+		}else if(one.start == theother.end){
+			return one.start;
+		}else if(one.end == theother.start){
+			return one.end;
+		}else if(one.end == theother.end){
+			return one.end;
+		}else{
+			throw std::runtime_error("given two sides doesn't share any points");
+		}
+	}
 	template <class P>
-	bool operator <(side<P>&,side<P>&);
+	bool operator<(side<P>& one,side<P>& theother){
+		return one.length() < theother.length();
+	}
 }
-#include"sides.cpp"
 #endif
