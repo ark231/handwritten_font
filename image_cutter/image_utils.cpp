62d87db9f2abd6f1abbd9f36aeda8fe4b77ee516
ark314 <arklp20909@gmail.com>
2021-04-05 15:08:08 +0900

    add: ニアリーイコール関数

--- image_cutter/image_utils.cpp
+++ image_cutter/image_utils.cpp
@@ -6,4 +6,17 @@ namespace handfont{
 	dpmm dpi_to_dpmm(dpi input_dpi){
 		return (input_dpi*inch_per_mm);
 	}
+	bool is_close(const double& one,const double& theother,const double aboutness,const close_type type){
+		double diff = abs((theother-one));
+		bool result = false;
+		switch(type){
+			case close_type::REL:
+				result = (diff < one * aboutness);
+				break;
+			case close_type::ABS:
+				result = (diff < aboutness);
+				break;
+		}
+		return result;
+	}
 }
--- image_cutter/image_utils.hpp
+++ image_cutter/image_utils.hpp
@@ -26,6 +26,11 @@ namespace handfont{
 	using dpi=double;
 	double constexpr inch_per_mm= 1/25.4;
 	dpmm dpi_to_dpmm(dpi input_dpi);
-
+	enum class close_type{
+		REL,//2つの差が、最初の引数に三番目の引数を掛けたもの未満かを判定
+		ABS//2つの差が、三番目の引数未満かを判定
+	};
+	double constexpr percent = 1.0/100;
+	bool is_close(const double&,const double&,const double,const close_type);
 }
 #endif
