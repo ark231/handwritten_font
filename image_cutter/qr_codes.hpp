ccde9df1e3434e5d420dceffebe0fbef15e4a4b4
ark314 <arklp20909@gmail.com>
2021-04-05 15:12:03 +0900

    change: 継承対応

--- image_cutter/qr_codes.hpp
+++ image_cutter/qr_codes.hpp
@@ -2,11 +2,12 @@
 #define HANDFONT_IMAGE_QR_CODES
 #include<string>
 #include<opencv2/core.hpp>
+#include"points.hpp"
 namespace handfont{
 	struct qr_code{
 		std::string data;
-		std::vector<cv::Point> vertice;
-		cv::Point center(){
+		std::vector<handfont::globalPoint> vertice;
+		cv::Point center() const noexcept{
 			cv::Point result(0,0);
 			for(const auto& vertex:vertice){
 				result.x+=vertex.x;
