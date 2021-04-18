f0300ef48ec644e67b274d150047fc096102fa81
ark314 <arklp20909@gmail.com>
2021-04-18 23:31:45 +0900

    add&change:bboxのサイズを返す処理を実装「プライベート」関数、変数を_始まりに

--- font_maker/svg_utils.py
+++ font_maker/svg_utils.py
@@ -2,17 +2,18 @@ import pathlib
 import svgelements
 class svg_handler:
     def __init__(self,filepath):
-        self.svg_data = svgelements.SVG.parse(filepath)
-    def calc_ex_rate(self,dst_width):
-        return dst_width/self.svg_data.width
+        self._svg_data = svgelements.SVG.parse(filepath)
+        self._calc_Bbox
+    def calc_ex_rate(self,dst_height): #プロポーショナルでも変わらないはずだから
+        return dst_height/self._svg_data.height
         pass
-    def calc_Bbox(self):
+    def _calc_Bbox(self):
         pathes=[]
-        for element in self.svg_data.elements():
+        for element in self._svg_data.elements():
             if isinstance(element,svgelements.Path):
                 pathes.append(element)
-        return svgelements.SVG.union_bbox(pathes)
-    def calc_width(self):
-        pass
-    def calc_height(self):
-        pass
+        self._Bbox = svgelements.SVG.union_bbox(pathes)
+    def get_width(self):
+        return self._Bbox[3]
+    def get_height(self):
+        return self._Bbox[2]
